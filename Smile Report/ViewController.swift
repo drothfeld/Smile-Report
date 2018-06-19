15a162d3066f6d80600a313f6c691937b7c4fd64
Dylan Rothfeld <drothfel@gmail.com>
2018-06-19 08:47:36 -0700

    inital commit

---
+++ Smile Report/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Smile Report
+//
+//  Created by Dylan Rothfeld on 6/19/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    // Hides status bar
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
+    // userDefaults custom data type storage: https:\\stackoverflow.com/questions/37980432/swift-3-saving-and-retrieving-custom-object-from-userdefaults
+
+}
+
