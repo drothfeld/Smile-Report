e6abdd14cd1c2474e209b9e9fbb6509face68eac
Dylan Rothfeld <drothfel@gmail.com>
2018-06-20 11:26:31 -0700

    home screen page control elements

--- Smile Report/View Controllers/HomeViewController.swift
+++ Smile Report/View Controllers/HomeViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    // Storyboard Outlets
+    @IBOutlet weak var PageControl: UIPageControl!
+    @IBOutlet weak var GraphScrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +27,7 @@ class HomeViewController: UIViewController {
 //    }
     
     // userDefaults custom data type storage: https:\\stackoverflow.com/questions/37980432/swift-3-saving-and-retrieving-custom-object-from-userdefaults
+    // Hooking up page control with scroll view: https:\\www.youtube.com/watch?v=X2Wr4TtMG6Q
 
 }
 
