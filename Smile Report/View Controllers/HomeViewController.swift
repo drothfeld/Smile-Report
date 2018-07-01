3a0b408d370abe04a456bd84736791221b03f41c
Dylan Rothfeld <drothfel@gmail.com>
2018-07-01 10:30:43 -0700

    date summary panel now populates with current datetime data

--- Smile Report/View Controllers/HomeViewController.swift
+++ Smile Report/View Controllers/HomeViewController.swift
@@ -12,6 +12,8 @@ class HomeViewController: UIViewController {
     // Storyboard Outlets
     @IBOutlet weak var PageControl: UIPageControl!
     @IBOutlet weak var GraphScrollView: UIScrollView!
+    @IBOutlet weak var CurrentWeekdayNameLabel: UILabel!
+    @IBOutlet weak var CurrentDateLabel: UILabel!
     
     // Controller Values
     var dayEntryData: [DayEntry] = mockData
@@ -24,6 +26,32 @@ class HomeViewController: UIViewController {
     func interfaceSetup() {
         // Changing status bar to white text
         UIApplication.shared.statusBarStyle = .lightContent
+        // Update summary panels
+        setDate()
+    }
+    
+    // Sets the current date panel to the actual current date
+    func setDate() {
+        // Getting date information
+        let currentDateTime = Date()
+        let userCalendar = Calendar.current
+        let requestedComponents: Set<Calendar.Component> = [
+            .year,
+            .month,
+            .day,
+            .hour,
+            .minute,
+            .second,
+        ]
+        let dateTimeComponents = userCalendar.dateComponents(requestedComponents, from: currentDateTime)
+        let currentYear = String(dateTimeComponents.year!) // 2018
+        let currentMonth = String(dateTimeComponents.month!) // 7
+        let currentDay = String(dateTimeComponents.day!) // 1
+        let formatter = DateFormatter()
+        let currentWeekday = String(formatter.weekdaySymbols[Calendar.current.component(.weekday, from: Date()) - 1])
+        // Setting outlet values
+        CurrentWeekdayNameLabel.text = currentWeekday
+        CurrentDateLabel.text = currentMonth + "/" + currentDay + "/" + currentYear
     }
     
     // userDefaults custom data type storage: https:\\stackoverflow.com/questions/37980432/swift-3-saving-and-retrieving-custom-object-from-userdefaults
