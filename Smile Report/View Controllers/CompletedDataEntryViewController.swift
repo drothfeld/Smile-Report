b4d109bb15559cbce856c5c5f8c79ca8531016c4
Dylan Rothfeld <drothfel@gmail.com>
2018-07-11 11:04:55 -0700

    segue timer auto transitions back to home view from completed data entry view

--- Smile Report/View Controllers/CompletedDataEntryViewController.swift
+++ Smile Report/View Controllers/CompletedDataEntryViewController.swift
@@ -13,14 +13,27 @@ class CompletedDataEntryViewController: UIViewController {
     
     // Controller Values
     var chosenSmile: Smile! = nil
+    var segueTimer: Timer = Timer()
+    let segueTimerValue: Double = 5.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         interfaceSetup()
+        startSegueTimer()
     }
     
     // Change any interface settings
     func interfaceSetup() {
+    }
+    
+    // Segue back to home after timer expires
+    @objc func segueToHome() {
+        performSegue(withIdentifier: "completedToHome", sender: self)
+    }
+    
+    // Starts segue timer
+    func startSegueTimer() {
+        segueTimer = Timer.scheduledTimer(timeInterval: segueTimerValue, target: self, selector: (#selector(CompletedDataEntryViewController.segueToHome)), userInfo: nil, repeats: false)
     }
 }
 
